9aaf5a927bc2a8399ff282616043e5b9adc6542f
Håkon Rossebø <hakonrossebo@hotmail.com>
2016-09-29 09:11:08 +0200

    Add action to show extra info

--- Main.elm
+++ Main.elm
@@ -1,6 +1,7 @@
 import Html.App as App
 import Html exposing (..)
 import Html.Attributes exposing (id, href, class, style)
+import Html.Events exposing (onClick)
 
 
 main : Program Never
@@ -16,17 +17,20 @@ main =
 
 type alias Model =
   { statusText : String
+  , showExtraInfo : Bool
   }
 
 
 model : Model
 model =
-  { statusText = "Ready"
- }
+  { statusText = "Showing extra info:"
+  , showExtraInfo = False
+  }
 
 
 type Msg
   = NoOp
+  | DoSomething
 
 
 
@@ -35,14 +39,39 @@ update msg model =
   case msg of
     NoOp ->
       ( model, Cmd.none )
+    DoSomething ->
+      ({ model | showExtraInfo = not model.showExtraInfo}, Cmd.none )
 
+
+
+statusHelper : Bool -> String
+statusHelper status =
+  case status of
+    True ->
+      " True"
+    False ->
+      " False"
 
 view : Model -> Html Msg
 view model =
-  div [][ text model.statusText
+  div [][ button [ onClick DoSomething ] [ text "Show/hide extra info" ]
+        , div [][text <| model.statusText ++ statusHelper model.showExtraInfo]
+        , viewExtraInfo model
         , div [id "chartdiv"][text "test"]
         ]
 
+
+viewExtraInfo : Model -> Html Msg
+viewExtraInfo model =
+  case model.showExtraInfo of
+    True ->
+      div [][ div [][ text "Test line 1"]
+            , div [][ text "Test line 2"]
+            , div [][ text "Test line 3"]
+            , div [][ text "Test line 4"]
+            ]
+    False ->
+      text ""
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
